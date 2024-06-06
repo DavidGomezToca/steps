b143aa87aa5e32c61e469350eee9ac4ac3467b4c
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2024-06-06 22:01:41 +0200

    STEPS 0.4.0
    Add step state
    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const messages = [
   "Fill a watering can with water",
   "Water the plants",
@@ -5,17 +7,24 @@ const messages = [
 ];
 
 export default function App() {
-  let step = 1;
+  const [step, setStep] = useState(1);
 
-  function handlePrevious() { alert("Previous") };
-  function handleNext() { alert("Next") };
+  function handlePrevious() {
+    if (step > 1)
+      setStep(step - 1);
+  };
+
+  function handleNext() {
+    if (step < 3)
+      setStep(step + 1);
+  };
 
   return <>
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">Step {step}: {messages[step - 1]}</p>
       <div className="buttons">
